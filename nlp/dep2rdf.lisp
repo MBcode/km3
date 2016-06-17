e3ee4b6cf6c19f4037bee3367a36108b9d99187c
MBcode <bobak@balisp.org>
2016-06-16 20:00:21 -0700

    dep2rdf.lisp

---
+++ nlp/dep2rdf.lisp
@@ -0,0 +1,6 @@
+;dep2rdf.lisp bobak@gmail
+;take stanford-parser/etc's dependency output from CoNLL-X format to a KM-like RDF/triples
+(lkm)
+(load "ls.cl" :print t)
+(load "lt.cl" :print t)
+(load-kb "sd.km")
